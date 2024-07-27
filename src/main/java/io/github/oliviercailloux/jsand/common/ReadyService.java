2cf409858c6540a99bcf057d24155e086f50bba9
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-07-27 21:35:03 +0200

    Transfer to JSand-common

--- src/main/java/io/github/oliviercailloux/jsand/common/ReadyService.java
+++
@@ -1,7 +0,0 @@
-package io.github.oliviercailloux.jsand.common;
-
-import java.rmi.Remote;
-
-public interface ReadyService extends Remote {
-  public void ready() throws java.rmi.RemoteException;
-}
